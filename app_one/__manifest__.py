7a6611fbc4b9095a31942984c11ad660113677a7
Merna Ibram naguib <99305257+Merna-ibram@users.noreply.github.com>
2025-04-22 18:05:43 +0200

    Update __manifest__.py(data)

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -7,6 +7,7 @@
     'depends': ['base', 'web', 'mail', 'sale_management', 'account_accountant', 'contacts'],
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/base_menu.xml',
         'views/property_view.xml',
         'views/owner_view.xml',

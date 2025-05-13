f709e1a41733ba101d9344c565b5793a49ba84d4
Merna Ibram naguib <99305257+Merna-ibram@users.noreply.github.com>
2025-05-13 05:41:29 +0300

    Add files via upload

---
+++ sale_image/__mainfest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sale Product Image',
+    'version': '1.0',
+    'category': 'Sales',
+    'depends': ['sale', 'base'],
+    'data': [
+        'views/sale_order_views.xml',
+        'report/sale_order_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

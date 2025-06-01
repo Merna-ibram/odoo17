84193f376227d13e53d40b5307b8ffef94dd8874
Merna Ibram naguib <99305257+Merna-ibram@users.noreply.github.com>
2025-06-01 14:11:03 +0300

    Add files via upload

---
+++ physiotherapy/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "physiotherapy",
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'application': True,
+
+    'depends': [
+        'base',
+    ],
+
+    'data': [
+        # Security
+        'security/ir.model.access.csv',
+
+
+        # Data
+
+
+        # Views
+        'views/base_menu.xml',
+        'views/registration_view.xml'
+
+
+
+        # Wizards
+
+
+        # Reports
+
+    ],
+
+
+}
